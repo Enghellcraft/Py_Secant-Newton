09a6e6a93da68994f43c029010c9347cdbc2b72c
starg09 <ultraguille2006@gmail.com>
2023-04-03 20:13:51 -0300

    Some fixes

--- Secant_Newton.py
+++ Secant_Newton.py
@@ -1,52 +1,63 @@
 from sympy import *
 
-#Symbol
+# Symbol
 x = symbols('x')
 
 MAX_STEPS_ALLOWED = 30
 
-def no_roots_found(f, x0, e, step):
+
+def no_roots_found(f, x0, f_x0, step):
     return print(
-        "\n\t[ERROR] No se pudo encontrar una raíz para la función \"%s\"" % f +
-        "tras %d pasos." % step +
+        "\n\t[ERROR] No se pudo encontrar una raíz para la función \" %s \" " % f +
+        "luego de %d pasos." % step +
         "\n\t\t* Último valor x analizado: %.4f" % x0 +
-        "\n\t\t* Último Epsilon: %.4f" % e
+        "\n\t\t* Ultimo f(x): %.4f" % f_x0
     )
 
-#Newton fun
+
+# Newton fun
 def my_newton(f, x0, e, step):
     # output es un estimado de la raiz de la funcion ingresada
     # Tiene implementada la funcion como recursiva
     # f = funcion ingresada
     # X0 = valor de inicio para la funcion (ideal cercano a la raiz)
     # e = epsilon o margen de error permitido
-    
+
     epsilon = e
-    
+
     step = step + 1
 
     x = symbols("x")
-    
+
     funcion = sympify(f)
-    
-    #Der Fun
-    df=funcion.diff(x)
-    
+
+    # Der Fun
+    df = funcion.diff(x)
+
     if df == 0:
         return print("No converge o lo hara con gran error ya que la derivada de la funcion es Cero")
-    
-    print("Newton: El valor encontrado en el paso ", step, "es", "%.4f" % x0, ". \n El valor de la funcion con ese valor es:", "~%.4f" % (funcion.subs({x: x0})).evalf())
 
-    if Abs(funcion.subs({x: x0}).evalf()) <= epsilon+0*I:
-        return print("La raiz es : ", "%.4f" % x0, ". Evaluada en la funcion es: ", "~%.4f" % (funcion.subs({x: x0})).evalf(), ". \n En: ", step, "pasos." )
+    f_x0 = funcion.subs({x: x0}).evalf()
+
+    print(
+        "\tNewton:\n"
+        "\t\tEl valor encontrado en el paso %d" % step + " es %.4f" % x0 + ".\n" +
+        "\t\tEl valor de la funcion con ese valor es: ~%.4f" % f_x0 +
+        "\n"
+    )
+
+    if Abs(funcion.subs({x: x0}).evalf()) <= epsilon + 0 * I:
+        return print("La raiz es : ", "%.4f" % x0, ". Evaluada en la funcion es: ",
+                     "~%.4f" % f_x0, ". \n En: ", step, "pasos.")
     else:
         if step >= MAX_STEPS_ALLOWED:
-            return no_roots_found(f, x0, epsilon, step)
+            return no_roots_found(f, x0, f_x0, step)
         x1 = x0 + 1
-        return my_secant(f, x0 - ((funcion/df).subs({x: x0}).evalf()), x0, epsilon, step)
+        return my_secant(f, x0 - ((funcion / df).subs({x: x0}).evalf()), x0, epsilon, step)
+
 
 # Secant Fun
-def my_secant(f,x0,x1,e, step):
+def my_secant(f, x0, x1, e, step):
     # output es un estimado de la raiz de la funcion ingresada
     # Tiene implementada la funcion como recursiva
     # f = funcion ingresada
@@ -62,21 +73,31 @@ def my_secant(f,x0,x1,e, step):
 
     funcion = sympify(f)
 
-    if (funcion.subs(x,x0)).evalf() == (funcion.subs(x,x1)).evalf():
+    if (funcion.subs(x, x0)).evalf() == (funcion.subs(x, x1)).evalf():
         return print('No puede dividir por cero!')
-    else: 
-        x2 = x0 - (x1-x0)*(funcion.subs(x,x0)).evalf()/( (funcion.subs(x,x1)).evalf() - (funcion.subs(x,x0)).evalf() ) 
-        
+    else:
+        x2 = x0 - (x1 - x0) * (funcion.subs(x, x0)).evalf() / (
+                (funcion.subs(x, x1)).evalf() - (funcion.subs(x, x0)).evalf())
+
         # x0 = x1
         # x1 = x2
-    
-        print("Secante: El valor encontrado en el paso ", step, "es", "%.4f" % x2, ". \n El valor de la funcion con ese valor es:", "~%.4f" % (funcion.subs(x,x2)).evalf())
 
-        if abs((funcion.subs(x,x2)).evalf()) <= e :
-            return print("La raiz es : ", "%.4f" % x2, ". Evaluada en la funcion es: ", "~%.4f" % (funcion.subs(x,x2)).evalf(), ". \n En: ", step, "pasos.")
+        f_x2 = funcion.subs(x, x2).evalf()
+
+        print(
+            "\tSecante:\n"
+            "\t\tEl valor encontrado en el paso %d" % step + " es %.4f" % x2 + "\n" +
+            "\t\tEl valor de la funcion con ese valor es: ~%.4f" % f_x2 +
+            "\n"
+        )
+
+
+        if abs((funcion.subs(x, x2)).evalf()) <= e:
+            return print("La raiz es : ", "%.4f" % x2, ". Evaluada en la funcion es: ",
+                         "~%.4f" % f_x2, ". \n En: ", step, "pasos.")
         else:
             if step >= MAX_STEPS_ALLOWED:
-                return no_roots_found(f, x0, epsilon, step)
+                return no_roots_found(f, x0, f_x2, step)
             return my_newton(f, x0, epsilon, step)
 
 
@@ -92,7 +113,7 @@ while option != 1 and option != 2:
     if option == 1:
         print("Ud selecciono el Metodo Newton \n")
     elif option == 2:
-        print("Ud selecciono el Metodo Secante \n")       
+        print("Ud selecciono el Metodo Secante \n")
     else:
         print("Ud ingreso un caracter invalido \n")
 step = 0
@@ -119,7 +140,7 @@ while epsilonChange != 1 and epsilonChange != 2:
                 print("Ud ingreso un caracter invalido. \n")
     elif epsilonChange == 2:
         epsilon = 0.0001
-    else :  
+    else:
         print("Ud ingreso un caracter invalido. \n")
 
 input_x0 = None
@@ -136,7 +157,7 @@ elif option == 2:
 print("")
 
 # Ingreso de funcion a los Metodos
-if option == 1: 
+if option == 1:
     my_newton(fun, input_x0, epsilon, step)
 elif option == 2:
     my_secant(fun, input_x0, input_x1, epsilon, step)
