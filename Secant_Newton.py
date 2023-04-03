cdbb6637744c48fd0b6ac9b9c881347bba84cb05
starg09 <ultraguille2006@gmail.com>
2023-04-03 19:40:47 -0300

    Add a step limit, alter some prints, remove unused imports

--- Secant_Newton.py
+++ Secant_Newton.py
@@ -1,9 +1,17 @@
-import numpy as np
-import sympy as sp
 from sympy import *
 
 #Symbol
 x = symbols('x')
+
+MAX_STEPS_ALLOWED = 30
+
+def no_roots_found(f, x0, e, step):
+    return print(
+        "\n\t[ERROR] No se pudo encontrar una raíz para la función \"%s\"" % f +
+        "tras %d pasos." % step +
+        "\n\t\t* Último valor x analizado: %.4f" % x0 +
+        "\n\t\t* Último Epsilon: %.4f" % e
+    )
 
 #Newton fun
 def my_newton(f, x0, e, step):
@@ -32,9 +40,11 @@ def my_newton(f, x0, e, step):
     if Abs(funcion.subs({x: x0}).evalf()) <= epsilon+0*I:
         return print("La raiz es : ", "%.4f" % x0, ". Evaluada en la funcion es: ", "~%.4f" % (funcion.subs({x: x0})).evalf(), ". \n En: ", step, "pasos." )
     else:
+        if step >= MAX_STEPS_ALLOWED:
+            return no_roots_found(f, x0, epsilon, step)
         x1 = x0 + 1
         return my_secant(f, x0 - ((funcion/df).subs({x: x0}).evalf()), x0, epsilon, step)
-   
+
 # Secant Fun
 def my_secant(f,x0,x1,e, step):
     # output es un estimado de la raiz de la funcion ingresada
@@ -64,17 +74,21 @@ def my_secant(f,x0,x1,e, step):
 
         if abs((funcion.subs(x,x2)).evalf()) <= e :
             return print("La raiz es : ", "%.4f" % x2, ". Evaluada en la funcion es: ", "~%.4f" % (funcion.subs(x,x2)).evalf(), ". \n En: ", step, "pasos.")
-        else :
+        else:
+            if step >= MAX_STEPS_ALLOWED:
+                return no_roots_found(f, x0, epsilon, step)
             return my_newton(f, x0, epsilon, step)
 
 
-#Input - User
+# Input - User
 print("*******************************************")
 print("Bienvenido al Programa de Busqueda de Ceros")
 print("*******************************************")
 option = -1
-while (option != 1 and option != 2):
-    option = int(input("¿Desea comenzar por el Metodo Newton (Presione 1) \n o por el Metodo de Secante (Presione 2) \n"))
+while option != 1 and option != 2:
+    option = int(input("¿Con qué metodo desea comenzar?\n"
+                       "\tComenzar por el Metodo Newton (Presione 1)\n"
+                       "\tComenzar por el Metodo de Secante (Presione 2)\n"))
     if option == 1:
         print("Ud selecciono el Metodo Newton \n")
     elif option == 2:
@@ -84,33 +98,44 @@ while (option != 1 and option != 2):
 step = 0
 fun = input("Ingrese su funcion \n")
 epsilon = 0.0001
-epsilonChange = -1
-while epsilonChange != 0 and epsilonChange != 1:
-    epsilonChange = int(input("El margen de error ha sido definido en 3 decimales, si desea cambiarlo ingrese 0, sino ingrese 1 \n"))
-    if epsilonChange == 1 :
+epsilonChange = 0
+while epsilonChange != 1 and epsilonChange != 2:
+    epsilonChange = int(input("El margen de error ha sido definido en 3 decimales:\n"
+                              "\tCambiarlo (Presione 1)\n"
+                              "\tDejarlo como está (Presione 2)\n"))
+    if epsilonChange == 1:
         epsilonOption = 0
         while epsilonOption != 1 and epsilonOption != 2:
-            epsilonOption = int(input("El margen de error puede cambiarse a 2 decimales Opcion 1, o 4 decimales Opcion 2 \n"))
+            epsilonOption = int(input("El margen de error puede cambiarse a:\n"
+                                      "\t2 decimales (Presione 1)\n"
+                                      "\t4 decimales (Presione 2)\n"))
             if epsilonOption == 1:
-                print("Ud selecciono 2 decimales \n")
+                print("Ud selecciono 2 decimales. \n")
                 epsilon = 0.001
             elif epsilonOption == 2:
-                print("Ud selecciono 4 decimales \n")
+                print("Ud selecciono 4 decimales. \n")
                 epsilon = 0.00001
-            else :  
-                print("Ud ingreso un caracter invalido \n")
-    elif epsilonChange == 0:
+            else:
+                print("Ud ingreso un caracter invalido. \n")
+    elif epsilonChange == 2:
         epsilon = 0.0001
     else :  
-        print("Ud ingreso un caracter invalido \n")
-    
-if option == 1: 
-    input_x0 = int(input ("Ingrese un valor X0 para comenzar \n"))
-elif option == 2:
-    input_x0 = int(input ("Ingrese un valor X0 inicial para comenzar \n"))
-    input_x1 = int(input ("Ingrese un valor X1 final para comenzar \n"))
+        print("Ud ingreso un caracter invalido. \n")
 
-#Ingreso de funcion a los Metodos
+input_x0 = None
+input_x1 = None
+
+if option == 1:
+    while input_x0 is None:
+        input_x0 = int(input("\nIngrese un valor X0 para comenzar: "))
+elif option == 2:
+    while input_x0 is None:
+        input_x0 = int(input("\nIngrese un valor X0 inicial para comenzar: "))
+    while input_x1 is None:
+        input_x1 = int(input("\nIngrese un valor X1 final para comenzar: "))
+print("")
+
+# Ingreso de funcion a los Metodos
 if option == 1: 
     my_newton(fun, input_x0, epsilon, step)
 elif option == 2:
